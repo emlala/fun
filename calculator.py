c5644806d922a02589a3045618f5334788c3ac9c
Emmi Lahtisalo <32639479+emlala@users.noreply.github.com>
2020-10-27 18:54:11 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+# This is a simple calculator program that I created to try out tkinter.
+# Running this program launches a calculator that can add, substract, divide and multiply.
+
 from tkinter import *
 
 root = Tk()
@@ -5,6 +8,8 @@ root.title("Simple Calculator")
 
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+# defining functions for the buttons
 
 def button_click(number):
     current = e.get()
@@ -21,22 +26,6 @@ def button_add():
     math = "addition"
     f_num = int(first_number)
     e.delete(0, END)
-
-def button_equal():
-    second_number = e.get()
-    e.delete(0, END)
-
-    if math == "addition":
-        e.insert(0, f_num + int(second_number))
-
-    if math == "substraction":
-        e.insert(0, f_num - int(second_number))
-
-    if math == "multiplication":
-        e.insert(0, f_num * int(second_number))
-
-    if math == "division":
-        e.insert(0, f_num / int(second_number))
 
 def button_substract():
     first_number = e.get()
@@ -61,6 +50,22 @@ def button_divide():
     math = "division"
     f_num = int(first_number)
     e.delete(0, END)
+    
+def button_equal():
+    second_number = e.get()
+    e.delete(0, END)
+
+    if math == "addition":
+        e.insert(0, f_num + int(second_number))
+
+    if math == "substraction":
+        e.insert(0, f_num - int(second_number))
+
+    if math == "multiplication":
+        e.insert(0, f_num * int(second_number))
+
+    if math == "division":
+        e.insert(0, f_num / int(second_number))
 
 # define buttons
 button1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
@@ -73,10 +78,10 @@ button7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(
 button8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+
 buttonAdd = Button(root, text="+", padx=39, pady=20, command=button_add)
 buttonEqual = Button(root, text="=", padx=87, pady=20, command=button_equal)
 buttonClear = Button(root, text="clear", padx=78, pady=20, command=button_clear)
-
 buttonSubtract = Button(root, text="-", padx=41, pady=20, command=button_substract)
 buttonMultiply = Button(root, text="*", padx=40, pady=20, command=button_multiply)
 buttonDivide = Button(root, text="/", padx=41, pady=20, command=button_divide)
