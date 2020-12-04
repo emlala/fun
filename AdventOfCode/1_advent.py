d44b1b729a10b2f0f834bce727a77b448b4aa959
Emmi Lahtisalo <32639479+emlala@users.noreply.github.com>
2020-12-04 21:46:15 +0200

    Advent of Code 2020 day 1

--- AdventOfCode/1_advent.py
+++ AdventOfCode/1_advent.py
@@ -1,8 +1,9 @@
 # Advent of Code 2020 day 1
 # https://adventofcode.com/
 
-# From 1_data.txt find the two entries that sum to 2020; the answer 1 is the two digits multiplied together.
-# Next find three entries that sum to 2020; the answer 2 is the three digits multiplied together.
+# TASK 1
+# From 1_data.txt find the two entries that sum to 2020.
+# Answer 1 is the two digits multiplied together.
 
 with open('1_data.txt') as f:
     lines = f.readlines()
@@ -17,17 +18,31 @@ key = 2020
 for i in range(0, len(lines)):
     n = key - lines[i]
     for j in range(i+1, len(lines)):
+        if lines[j] == n:
+            a1 = lines[i]
+            a2 = lines[j]
+        else:
+            continue
+
+answer1 = a1*a2
+print("Answer 1 is", answer1)
+
+
+# TASK 2
+# Next find three entries that sum to 2020.
+# Answer 2 is the three digits multiplied together.
+
+for i in range(0, len(lines)):
+    n = key - lines[i]
+    for j in range(i+1, len(lines)):
         m = n - lines[j]
         for k in range(j+1, len(lines)):
             if lines[k] == m:
-                a1 = lines[i]
-                a2 = lines[j]
-                a3 = lines[k]
+                b1 = lines[i]
+                b2 = lines[j]
+                b3 = lines[k]
             else:
                 continue
 
-answer1 = a1*a2
-answer2 = a1*a2*a3
-
-print("Answer 1 is", answer1)
+answer2 = b1*b2*b3
 print("Answer 2 is", answer2)
