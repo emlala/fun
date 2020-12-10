9044be1895caa8ed0d4c24ca55bb045833a14391
Emmi Lahtisalo <32639479+emlala@users.noreply.github.com>
2020-12-10 18:13:25 +0200

    Advent of Code 2020 day 9 part 2 added

--- AdventOfCode/9_advent.py
+++ AdventOfCode/9_advent.py
@@ -13,18 +13,53 @@ slice = []
 y = 25
 x = 0
 
-# Creating a list of all possible sums of two numbers in the range 0-24, and checking if 25 is in the list.
-# If it is, clearing the list and doing the same to numbers 1-25 and 26.
-# Continuing until the program finds a number that is not in the list of sums of the previous 25.
+# Creating a list of all possible sums of two numbers between list indices 0-24
+# Checking if one of the sums equals index 25.
+# If it doesn't, the value of index 25 is our answer.
+# If it does, clearing the list and doing the same comparison to numbers 1-25 and 26.
 
 for i in range(len(lines)):
     for j in range(x, y):
         for k in range(x+1, y):
             slice.append(lines[j]+lines[k])
     if lines[j+1] not in slice:
-        print("The answer is", lines[j+1])
+        key = lines[j+1]
+        print("The answer to task 1 is", key)
         break
     else:
         x += 1
         y += 1
         slice.clear()
+
+
+# TASK 2
+
+vals = []
+
+for i in range(len(lines)):
+
+    if sum(vals) == key:
+        break
+
+    else:
+        # Adding each value to a list to compare the sum of the values to key.
+        vals.append(lines[i])
+        for j in range(i+1, len(lines)):
+            vals.append(lines[j])
+            s = sum(vals)
+            # If sum is larger than key, empty the value list and start again from i+1.
+            if s > key:
+                vals.clear()
+                break
+            # If sum is less than key, keep adding.
+            elif s < key:
+                continue
+            # If sum equals key, we have our list of values.
+            else:
+                break
+
+
+# The answer is the sum of the largest and the smallest value in the list.
+
+vals.sort()
+print("The answer to task 2 is", vals[0]+vals[-1])
